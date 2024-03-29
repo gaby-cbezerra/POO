6b8fe8cbf111eaf00e772b166b74e2dc48c40b17
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-03-29 18:38:56 -0300

    Intervalo 2
    Questão 1072 do beecrowd

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int x = 10;
-            for (int i = 1; i <= 10 ; i++)
-            {
-                System.Console.Write(i + " ");
-            }
-        }
+int numRep = int.Parse(Console.ReadLine());
+int dentro = 0;
+int fora = 0;
+for ( int n = 0; n < numRep; n++){
+    int x = int.Parse(Console.ReadLine());
+    if ( x >= 10 && x <= 20){
+        dentro++;
     }
+    else 
+        fora++;
+    
 }
+Console.WriteLine($"{dentro} in");
+Console.WriteLine($"{fora} out");

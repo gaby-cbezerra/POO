16c312955822d8151367b49ed4465b009c8096b3
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-03-30 11:33:05 -0300

    Quadrado de Pares
    Questão 1073 no beecrowd

--- Program.cs
+++ Program.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 
-int numRep = int.Parse(Console.ReadLine());
-int dentro = 0;
-int fora = 0;
-for ( int n = 0; n < numRep; n++){
-    int x = int.Parse(Console.ReadLine());
-    if ( x >= 10 && x <= 20){
-        dentro++;
-    }
-    else 
-        fora++;
-    
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++){
+if( i%2 == 0){
+Console.WriteLine($"{i}^{2} = {i*i}");
 }
-Console.WriteLine($"{dentro} in");
-Console.WriteLine($"{fora} out");
+}

521708e639b78b2372a072619664aabfe331fa51
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-03-31 18:30:14 -0300

    Sequência IJ 2
    Questão 1096 do beecrowd

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
 
-int I= 1;
-int J= 60;
-while( J >= 0){
-    Console.WriteLine($"I={I} J={J}");
-    I+=3;
-    J-=5;
-}
+int i = 1;
+ while(i == 1){
+    Console.WriteLine($"I=1 J=7");
+    Console.WriteLine($"I=1 J=6");
+    Console.WriteLine($"I=1 J=5");
+    i++;
+ }
+ int I = 1;
+ while(I == 1){
+    Console.WriteLine($"I=3 J=7");
+    Console.WriteLine($"I=3 J=6");
+    Console.WriteLine($"I=3 J=5");
+    I++;
+ }
+
+int j = 1;
+ while(j == 1){
+    Console.WriteLine($"I=5 J=7");
+    Console.WriteLine($"I=5 J=6");
+    Console.WriteLine($"I=5 J=5");
+    j++;
+ }
+
+int J = 1;
+ while(J == 1){
+    Console.WriteLine($"I=7 J=7");
+    Console.WriteLine($"I=7 J=6");
+    Console.WriteLine($"I=7 J=5");
+    J++;
+ }
+int A = 1;
+ while(A == 1){
+    Console.WriteLine($"I=9 J=7");
+    Console.WriteLine($"I=9 J=6");
+    Console.WriteLine($"I=9 J=5");
+    A++;
+ }

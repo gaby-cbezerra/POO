386c20de08d371e19f64f3e65409f833f32b3a7f
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-03-31 18:43:53 -0300

    Sequência IJ 3
    Questão 1097 do beecrowd

--- Program.cs
+++ Program.cs
@@ -1,39 +1,11 @@
 ﻿using System;
 
-int i = 1;
- while(i == 1){
-    Console.WriteLine($"I=1 J=7");
-    Console.WriteLine($"I=1 J=6");
-    Console.WriteLine($"I=1 J=5");
-    i++;
- }
- int I = 1;
- while(I == 1){
-    Console.WriteLine($"I=3 J=7");
-    Console.WriteLine($"I=3 J=6");
-    Console.WriteLine($"I=3 J=5");
-    I++;
- }
-
-int j = 1;
- while(j == 1){
-    Console.WriteLine($"I=5 J=7");
-    Console.WriteLine($"I=5 J=6");
-    Console.WriteLine($"I=5 J=5");
-    j++;
- }
-
-int J = 1;
- while(J == 1){
-    Console.WriteLine($"I=7 J=7");
-    Console.WriteLine($"I=7 J=6");
-    Console.WriteLine($"I=7 J=5");
-    J++;
- }
-int A = 1;
- while(A == 1){
-    Console.WriteLine($"I=9 J=7");
-    Console.WriteLine($"I=9 J=6");
-    Console.WriteLine($"I=9 J=5");
-    A++;
- }
+int i=1, j=7;
+		while (i <= 9) {
+			for (int k = 0; k < 3; k++) {
+				Console.WriteLine("I=" + i + " J=" + j);
+				j--;
+			}
+			j+=5;
+			i+=2;
+        }

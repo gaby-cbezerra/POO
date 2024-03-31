7c6e773afff834adaf60668fe658def6c0ef7986
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-03-31 16:32:05 -0300

    Maior e Posição
    Questão 1080 do beecrowd

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 
-int n = int.Parse(Console.ReadLine());
+int maior = 0;
+int posMaior = 1;
+int x;
+    for (int i = 1; i <= 100; i++) {
+	 x = int.Parse(Console.ReadLine());
+			if (i == 1) {
+				maior = x;
+				posMaior = 1;
+			} else if (x > maior) {
+				maior = x;
+				posMaior = i;
+			}
+		}
+Console.WriteLine(maior);
+Console.WriteLine(posMaior);
+	
 
-for (int i = 1; i <= 10; i++){
-    Console.WriteLine($"{i} x {n} = {i*n}");
-    
-}
+
+
+
 
     
     

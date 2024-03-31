b863f4452f0eb8627b185c73e53f46ebcacca6c4
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-03-31 17:01:45 -0300

    Sequência IJ 1
    Questão 1095 do beecrowd

--- Program.cs
+++ Program.cs
@@ -1,25 +1,9 @@
 ﻿using System;
 
-int maior = 0;
-int posMaior = 1;
-int x;
-    for (int i = 1; i <= 100; i++) {
-	 x = int.Parse(Console.ReadLine());
-			if (i == 1) {
-				maior = x;
-				posMaior = 1;
-			} else if (x > maior) {
-				maior = x;
-				posMaior = i;
-			}
-		}
-Console.WriteLine(maior);
-Console.WriteLine(posMaior);
-	
-
-
-
-
-
-    
-    
+int I= 1;
+int J= 60;
+while( J >= 0){
+    Console.WriteLine($"I={I} J={J}");
+    I+=3;
+    J-=5;
+}

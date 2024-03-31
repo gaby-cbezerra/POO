15900e79cd28fba4e646f7191163ff2bd585d397
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-03-31 15:54:11 -0300

    Tabuada
    Questão 1078 do beecrowd

--- Program.cs
+++ Program.cs
@@ -2,10 +2,9 @@
 
 int n = int.Parse(Console.ReadLine());
 
-for (int i = 1; i < 10000; i++){
-    if(i % n == 2){
-        Console.WriteLine(i);
-    }
+for (int i = 1; i <= 10; i++){
+    Console.WriteLine($"{i} x {n} = {i*n}");
+    
 }
 
     

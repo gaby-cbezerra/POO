56ee3b258a53d352f01930ed8b4afce734cf5270
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-04-04 11:51:26 -0300

    Animal
    Questão 1049 do beecrowd

--- Program.cs
+++ Program.cs
@@ -1,11 +1,61 @@
 ﻿using System;
 
-int i=1, j=7;
-		while (i <= 9) {
-			for (int k = 0; k < 3; k++) {
-				Console.WriteLine("I=" + i + " J=" + j);
-				j--;
-			}
-			j+=5;
-			i+=2;
+string n1 = Console.ReadLine();
+string n2 = Console.ReadLine();
+string n3 = Console.ReadLine();
+    if ( n1 == "vertebrado"){
+        if(n2 == "ave"){
+            if(n3 == "carnivoro"){
+                Console.WriteLine("aguia");
+            }
         }
+    }
+    if ( n1 == "vertebrado"){
+        if(n2 == "ave"){
+            if(n3 == "onivoro"){
+                Console.WriteLine("pomba");
+            }
+        }
+    }
+    if ( n1 == "vertebrado"){
+        if(n2 == "mamifero"){
+            if(n3 == "onivoro"){
+                Console.WriteLine("homem");
+            }
+        }
+    }
+    if ( n1 == "vertebrado"){
+        if(n2 == "mamifero"){
+            if(n3 == "herbivoro"){
+                Console.WriteLine("vaca");
+            }
+        }
+    }
+    if ( n1 == "invertebrado"){
+        if(n2 == "inseto"){
+            if(n3 == "hematofago"){
+                Console.WriteLine("pulga");
+            }
+        }
+    }
+    if ( n1 == "invertebrado"){
+        if(n2 == "inseto"){
+            if(n3 == "herbivoro"){
+                Console.WriteLine("lagarta");
+            }
+        }
+    }
+    if ( n1 == "invertebrado"){
+        if(n2 == "anelideo"){
+            if(n3 == "hematofago"){
+                Console.WriteLine("sanguessuga");
+            }
+        }
+    }
+    if ( n1 == "invertebrado"){
+        if(n2 == "anelideo"){
+            if(n3 == "onivoro"){
+                Console.WriteLine("minhoca");
+            }
+        }
+    }
